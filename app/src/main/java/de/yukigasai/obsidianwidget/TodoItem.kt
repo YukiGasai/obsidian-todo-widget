2a9b1f79ac02a7de4558c05fe07a2e783215c258
YukiGasai <r.lindede@googlemail.com>
2023-09-04 20:06:47 +0200

    Allow darkmode on SDK < 31

--- app/src/main/java/de/yukigasai/obsidianwidget/TodoItem.kt
+++ app/src/main/java/de/yukigasai/obsidianwidget/TodoItem.kt
@@ -1,3 +1,3 @@
 package de.yukigasai.obsidianwidget
 
-data class TodoItem(val id: Int, val name:String, var isChecked: Boolean, val offSet: String)
+data class TodoItem(val id: Int, val name:String, var isChecked: Boolean, var offSet: String)
--- app/src/main/java/de/yukigasai/obsidianwidget/TodoWidget.kt
+++ app/src/main/java/de/yukigasai/obsidianwidget/TodoWidget.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
+import android.os.Build
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -11,6 +12,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.glance.BackgroundModifier
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
@@ -21,12 +23,14 @@ import androidx.glance.action.ActionParameters
 import androidx.glance.action.actionParametersOf
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.CheckBox
+import androidx.glance.appwidget.CheckboxDefaults
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.action.ActionCallback
 import androidx.glance.appwidget.action.ToggleableStateKey
 import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
@@ -35,6 +39,7 @@ import androidx.glance.background
 import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
+import androidx.glance.layout.ContentScale
 import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
@@ -51,11 +56,23 @@ class TodoWidget : GlanceAppWidget() {
         provideContent { Content() }
     }
 
+    private fun GlanceModifier.appWidgetBackgroundCornerRadius(): GlanceModifier {
+        if (Build.VERSION.SDK_INT >= 31) {
+            return cornerRadius(android.R.dimen.system_app_widget_background_radius)
+        }
+
+        return this.then(
+            BackgroundModifier(
+                ImageProvider(R.drawable.background_widget), ContentScale.Fit)
+        )
+    }
+
     @Composable
     fun Content() {
         val todos by TodoRepo.currentTodos.collectAsState()
         val context = LocalContext.current
         val config = ListSharedPrefsUtil.loadWidgetSettings(context)
+
         GlanceTheme {
             Column(
                 modifier = GlanceModifier
@@ -63,6 +80,7 @@ class TodoWidget : GlanceAppWidget() {
                     .appWidgetBackground()
                     .padding(4.dp)
                     .background(GlanceTheme.colors.background)
+                    .appWidgetBackgroundCornerRadius()
             ) {
                 Row(
                     modifier = GlanceModifier.fillMaxWidth().padding(4.dp),
@@ -115,10 +133,13 @@ class TodoWidget : GlanceAppWidget() {
 private fun CheckBoxItem(item: TodoItem, config: WidgetConfig) {
     val prefs = currentState<Preferences>()
     val checked = prefs[booleanPreferencesKey(item.id.toString())] ?: item.isChecked
+    item.offSet = item.offSet.ifEmpty { " " }
 
     CheckBox(
         text = item.name,
+        style = TextStyle(GlanceTheme.colors.onBackground),
         checked = checked,
+        colors = CheckboxDefaults.colors(),
         onCheckedChange = actionRunCallback<CheckboxClickAction>(
             actionParametersOf(
                 toggledItemKey to item.id.toString()
