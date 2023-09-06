6f3321384d3d3965158222c2c0c69e937e065f82
YukiGasai <r.lindede@googlemail.com>
2023-09-07 00:08:48 +0200

    Version Bump + Allow @hh:mm as reminder Date

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.yukigasai.obsidiantodowidget"
         minSdk = 26
         targetSdk = 33
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/de/yukigasai/obsidiantodowidget/TodoWidget.kt
+++ app/src/main/java/de/yukigasai/obsidiantodowidget/TodoWidget.kt
@@ -60,6 +60,7 @@ import kotlinx.coroutines.runBlocking
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 class TodoWidget : GlanceAppWidget() {
 
@@ -215,22 +216,31 @@ class RefreshButtonHandler : ActionCallback {
 }
 @SuppressLint("ScheduleExactAlarm")
 fun checkForReminders(todo: TodoItem, context: Context) {
+    val nowDateTime = LocalDateTime.now()
+
     // Get The dateTime from the name
-    val getDateRegex = Regex("[@\uD83D\uDCC5]\\{?(\\d{4}-\\d{2}-\\d{2}( \\d{2}:\\d{2})?)\\}?")
+    val getDateRegex = Regex("[@\uD83D\uDCC5]\\{?((\\d{4}-\\d{2}-\\d{2})?(\\s?\\d{2}:\\d{2})?)\\}?")
     // Find date match or return
     val match: MatchResult = getDateRegex.find(todo.name) ?: return
-    var dateString = match.groups[1]?.value
 
-    // Add time if none is set
-    if (match.groupValues[0].length < 16) {
-        dateString = "$dateString 12:00"
+
+    var dateString = match.groups[2]?.value ?: nowDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+
+    dateString += if(match.groups[3] == null){
+        " 12:00"
+    }else {
+        " ${match.groups[3]?.value?.trim()}"
     }
 
     val todoTileWithoutDate = todo.name.replace(match.groupValues[0], "")
-
     // Calculate the milliseconds  until reminder should fire
-    val reminderDateTime = LocalDateTime.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
-    val nowDateTime = LocalDateTime.now()
+    val reminderDateTime:LocalDateTime
+    try {
+        reminderDateTime = LocalDateTime.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+    }catch (error: DateTimeParseException) {
+        System.err.println(error.cause)
+        return
+    }
     val duration = Duration.between(nowDateTime, reminderDateTime)
 
     // Event already happened
