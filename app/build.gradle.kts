4980751bcc5f7640abc186d6ff99e22418c53c01
YukiGasai <r.lindede@googlemail.com>
2023-09-17 13:20:43 +0200

    Allow regex special char in todo name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.yukigasai.obsidiantodowidget"
         minSdk = 26
         targetSdk = 33
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 7
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/de/yukigasai/obsidiantodowidget/todo/TodoItem.kt
+++ app/src/main/java/de/yukigasai/obsidiantodowidget/todo/TodoItem.kt
@@ -2,15 +2,16 @@ package de.yukigasai.obsidiantodowidget.todo
 
 import de.yukigasai.obsidiantodowidget.util.FsHelper
 import de.yukigasai.obsidiantodowidget.WidgetConfig
+import kotlin.text.Regex.Companion.escape
 
 data class TodoItem(val id: Int, val name:String, var isChecked: Boolean, var offSet: String) {
     fun updateInFile(config: WidgetConfig) {
         val originalFile = FsHelper.loadTextData(config)
         val newFileData: String = if(this.isChecked){
-            val regex = Regex("- \\[x] ${this.name}\$", RegexOption.MULTILINE)
+            val regex = Regex("- \\[x] ${escape(this.name)}\$", RegexOption.MULTILINE)
             originalFile.replace(regex, "- [ ] ${this.name}")
         }else {
-            val regex = Regex("- \\[ ] ${this.name}\$", RegexOption.MULTILINE)
+            val regex = Regex("- \\[ ] ${escape(this.name)}\$", RegexOption.MULTILINE)
             originalFile.replace(regex, "- [x] ${this.name}")
         }
         FsHelper.overwriteFile(config.getFullPath(), newFileData)
